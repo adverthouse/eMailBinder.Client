a1929c836c1a429a3d95987e97b7e3a0b766c2c4
yunusozturk@msn.com <yunusozturk@msn.com>
2024-07-09 17:22:37 +0300

    Black list changes

--- eMailBinder.Client/Common/StatusCode.cs
+++ eMailBinder.Client/Common/StatusCode.cs
@@ -4,15 +4,16 @@ public enum StatusCode{
     Ok = 200,
     Unsubscribed = 201,
     AlreadySubscribed = 202,
-    InvalidEmailAddress = 203,
-    
-
-
+    InvalidEmailAddress = 203,  
     Error = 400,
     InvalidAPIKey = 500,
     APIKeyNotPassed = 501,
-    SubscriptionListNotFound = 502 ,
+    SubscriptionListNotFound = 502,
     EmailAddressDoesntExists = 503,
-    EmailAddressAlreadyConfirmed = 504 
-
+    EmailAddressAlreadyConfirmed = 504,
+    EmailTemplateNotFound = 505,
+    ScheduleDateNotPassed = 506,
+    ScheduleDateNotValid = 507,
+    CampaignNameRequired = 508,
+    BlacklistEmailAddress = 509
 }
--- eMailBinder.TestClient/Program.cs
+++ eMailBinder.TestClient/Program.cs
@@ -5,18 +5,18 @@ internal class Program
     {
         Console.WriteLine("Hello, World!");
 
-        eMailBinder.Client.eMailBinder binder = new("https://emailbinder.yunus.us","45795dd61a204856b41fbb0ccde653db");
+        eMailBinder.Client.eMailBinder binder = new("https://localhost:5100","fbf397b2f81c4bf0b24b9e886051b794");
 
         var result = await binder.SubscribeToList(new eMailBinder.Client.Requests.SubscriptionRequest(){
              EmailAddress = "yunusozturk@gmail.com",
-             SubscriptionListSlug = "main_list",
+             SubscriptionListSlug = "main-list",
              Name = "Yunus",
              IPAddress = "192.168.1.1"
         });
-/*
+
         Console.WriteLine($"SubscribeToList : {result.StatusCode}, {result.StatusMessage}"); 
 
-        result = await binder.VerifyEmailAddress(new eMailBinder.Client.Requests.VerifyEmailAddressRequest(){
+     /*   result = await binder.VerifyEmailAddress(new eMailBinder.Client.Requests.VerifyEmailAddressRequest(){
            VerificationCode = "8Baz/KfrlNipBURKW2Eu9FcYJ75dAU8zjqzyMsIioiINwdA8lDvbi+Cszoa0Rx79"
         });
 
